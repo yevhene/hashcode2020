e6acea0079ec0449afa0f7c6efacbfb772ed2429
Paul Druziak <paul.druziak@icloud.com>
2020-02-20 20:31:41 +0200

    add score method

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require_relative './library.rb'
 
 number_of_books, number_of_libs, deadline = gets.split(' ').map(&:to_i)
 
-books = gets.split(' ').map(&:to_i)
+$books = gets.split(' ').map(&:to_i)
 
-fail if books.size != number_of_books
+fail if $books.size != number_of_books
 
 libraries = []
 
@@ -12,5 +12,6 @@ number_of_libs.times do |i|
   lib_id, signup_time, can_ship_books = gets.split(' ').map(&:to_i)
   books_in_lib = gets.split(' ').map(&:to_i)
   library = Library.new lib_id, signup_time, can_ship_books, books_in_lib
+  p library.score
   libraries << library
 end
--- library.rb
+++ library.rb
@@ -7,4 +7,10 @@ class Library
     @can_ship_books = can_ship_books
     @books_in_lib = books_in_lib
   end
+
+  def score
+    books_value = books_in_lib.inject(0) { |sum, book_id| sum + $books[book_id]  }
+    books_value_per_day = books_value / can_ship_books
+    books_value_per_day / signup_time
+  end
 end
