df223a63ca7e0f54dbdb920fd9e6c8db71c42cf1
Paul Druziak <paul.druziak@icloud.com>
2020-02-20 20:26:03 +0200

    update parser

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
+require_relative './library.rb'
+
 number_of_books, number_of_libs, deadline = gets.split(' ').map(&:to_i)
 
 books = gets.split(' ').map(&:to_i)
 
+fail if books.size != number_of_books
+
+libraries = []
+
 number_of_libs.times do |i|
   lib_id, signup_time, can_ship_books = gets.split(' ').map(&:to_i)
   books_in_lib = gets.split(' ').map(&:to_i)
+  library = Library.new lib_id, signup_time, can_ship_books, books_in_lib
+  libraries << library
 end
--- library.rb
+++ library.rb
@@ -1,3 +1,10 @@
 class Library
-  attr_reader :id
+  attr_reader :lib_id, :signup_time, :can_ship_books, :books_in_lib
+
+  def initialize(lib_id, signup_time, can_ship_books, books_in_lib)
+    @lib_id = lib_id
+    @signup_time = signup_time
+    @can_ship_books = can_ship_books
+    @books_in_lib = books_in_lib
+  end
 end
