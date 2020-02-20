7c81591d1fcd757695c037f59ea786b562515d64
Yevhen Shemet <yevhene@gmail.com>
2020-02-20 20:39:57 +0200

    Processing boilerplate

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require_relative './library.rb'
 
-number_of_books, number_of_libs, deadline = gets.split(' ').map(&:to_i)
+number_of_books, number_of_libs, days = gets.split(' ').map(&:to_i)
 
-$books = gets.split(' ').map(&:to_i)
+books = gets.split(' ').map(&:to_i)
 
-fail if $books.size != number_of_books
+fail if books.size != number_of_books
 
 libraries = []
 
@@ -12,6 +12,7 @@ number_of_libs.times do |i|
   lib_id, signup_time, can_ship_books = gets.split(' ').map(&:to_i)
   books_in_lib = gets.split(' ').map(&:to_i)
   library = Library.new lib_id, signup_time, can_ship_books, books_in_lib
-  p library.score
   libraries << library
 end
+
+processing = Processing.new(libraries, books, days)
---
+++ processing.rb
@@ -0,0 +1,7 @@
+class Processing
+  def initialize(libraries, books, days)
+    @libraries = libraries
+    @books = books
+    @days = days
+  end
+end
