3e64fc9aa7e509ca9b4c510a0a31ba942921e33a
Paul Druziak <paul.druziak@icloud.com>
2020-02-20 21:12:40 +0200

    shell

---
+++ run.sh
@@ -0,0 +1,6 @@
+ruby app.rb < in/a_example.txt > out/a_example.txt
+ruby app.rb < in/b_read_on.txt > out/b_read_on.txt
+ruby app.rb < in/c_incunabula.txt > out/c_incunabula.txt
+ruby app.rb < in/d_tough_choices.txt > out/d_tough_choices.txt
+ruby app.rb < in/e_so_many_books.txt > out/e_so_many_books.txt
+ruby app.rb < in/f_libraries_of_the_world.txt > out/f_libraries_of_the_world.txt
